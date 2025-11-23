5ee11c3b1edd360543f316c48269e71a61e39409
theBappy <abappy575@gmail.com>
2025-11-23 17:25:12 +0600

    safari browser implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap"
 import { Draggable } from "gsap/Draggable"
 
 import {Navbar, Welcome, Dock} from "#components"
-import { Terminal } from "#windows"
+import { Safari, Terminal } from "#windows"
 
 gsap.registerPlugin(Draggable)
 
@@ -12,7 +12,9 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
+      
       <Terminal />
+      <Safari />
     </main>
   )
 }
